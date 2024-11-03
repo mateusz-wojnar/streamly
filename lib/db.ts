17a2f473f5d74dcdc55b9b5ba8263fd08550a222
mateusz-wojnar <mwojnar87@gmail.com>
2024-11-03 15:04:51 +0100

    Integrate prisma with neon.tech postgresql db

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+//prevent creating unnecessary prisma clients by hot reloading (not in prod)
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
