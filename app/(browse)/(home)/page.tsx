7ae132271ef218994001e512f8a0073562dc11e5
mateusz-wojnar <mwojnar87@gmail.com>
2024-11-07 20:23:55 +0100

    completed implementing navbar with search, actions and logo

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,10 +1,9 @@
-import {UserButton} from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
-      <h1>Dashboard</h1>
-      <UserButton/>
+      <h1>Home page</h1>
     </div>
   );
 }
