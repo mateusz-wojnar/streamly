0299deb5e8f2c294596bc49323968695052055e6
mateusz-wojnar <mwojnar87@gmail.com>
2024-11-08 23:54:26 +0100

    implemented recommended list in sidebar, added skeletons

---
+++ lib/auth-service.ts
@@ -0,0 +1,23 @@
+import { currentUser } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+import { Simulate } from "react-dom/test-utils";
+import error = Simulate.error;
+
+export const getSelf = async () => {
+  const self = await currentUser();
+
+  if (!self || !self.username) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalUserId: self.id },
+  });
+
+  if (!user) {
+    throw new Error("Not found");
+  }
+
+  return user;
+};
