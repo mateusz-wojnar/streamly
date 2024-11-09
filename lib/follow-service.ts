c5f1e91ba4a97a65053b285f8c54ce42f478b8cf
mateusz-wojnar <mwojnar87@gmail.com>
2024-11-09 16:30:10 +0100

    Finished implementing blocking functionality

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -1,25 +1,31 @@
 import { db } from "@/lib/db";
 import { getSelf } from "@/lib/auth-service";
 
-
 export const getFollowedUsers = async () => {
   try {
     const self = await getSelf();
 
     const followedUsers = db.follow.findMany({
       where: {
-        followerId: self.id
+        followerId: self.id,
+        following: {
+          blocking: {
+            none: {
+              blockedId: self.id,
+            },
+          },
+        },
       },
-      include:  {
+      include: {
         following: true,
-      }
-    })
+      },
+    });
 
-    return followedUsers
+    return followedUsers;
   } catch {
     return [];
   }
-}
+};
 
 export const isFollowingUser = async (id: string) => {
   try {
