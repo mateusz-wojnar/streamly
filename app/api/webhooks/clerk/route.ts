a7a5720c75b8784b9c7f08f607d8229ead10974d
mateusz-wojnar <mwojnar87@gmail.com>
2024-11-11 17:44:50 +0100

    created stream model and modified webhook to create stream at user creation

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,95 +1,99 @@
-import {Webhook} from "svix";
-import {headers} from "next/headers";
-import {WebhookEvent} from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
 
-import {db} from '@/lib/db'
+import { db } from "@/lib/db";
 
-export async function POST(req: Request ) {
-    const  WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
-    if (!WEBHOOK_SECRET) {
-        throw new Error('Add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env')
+  if (!WEBHOOK_SECRET) {
+    throw new Error("Add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env");
+  }
+
+  //GET HEADERS
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  //NO HEADERS ERROR
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  //GET THE BODY
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  //CREATE NEW SVIX INSTANCE
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  //VERIFY PAYLOAD WITH HEADERS
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+
+  //GET ID AND TYPE
+  const eventType = evt.type;
+
+  if (eventType === "user.created") {
+    await db.user.create({
+      data: {
+        externalUserId: payload.data.id,
+        username: payload.data.username,
+        imageUrl: payload.data.image_url,
+        stream: {
+          create: {
+            name: `${payload.data.username}'s stream`,
+          },
+        },
+      },
+    });
+  }
+
+  if (eventType === "user.updated") {
+    const currentUser = await db.user.findUnique({
+      where: {
+        externalUserId: payload.data.id,
+      },
+    });
+
+    if (!currentUser) {
+      return new Response("User not found", { status: 404 });
     }
 
-    //GET HEADERS
-    const headerPayload = await headers()
-    const svix_id = headerPayload.get("svix-id")
-    const svix_timestamp = headerPayload.get('svix-timestamp')
-    const svix_signature = headerPayload.get('svix-signature')
+    await db.user.update({
+      where: {
+        externalUserId: payload.data.id,
+      },
+      data: {
+        username: payload.data.username,
+        imageUrl: payload.data.image_url,
+      },
+    });
+  }
 
-    //NO HEADERS ERROR
-    if (!svix_id || !svix_timestamp || !svix_signature) {
-        return new Response('Error occured -- no svix headers', {
-            status:  400
-        })
-    }
+  if (eventType === "user.deleted") {
+    await db.user.delete({
+      where: {
+        externalUserId: payload.data.id,
+      },
+    });
+  }
 
-    //GET THE BODY
-    const payload = await req.json()
-    const body = JSON.stringify(payload)
-
-    //CREATE NEW SVIX INSTANCE
-    const wh = new Webhook(WEBHOOK_SECRET)
-
-    let evt: WebhookEvent
-
-    //VERIFY PAYLOAD WITH HEADERS
-    try {
-        evt = wh.verify(body, {
-            "svix-id": svix_id,
-            "svix-timestamp": svix_timestamp,
-            "svix-signature": svix_signature,
-        }) as WebhookEvent
-    } catch (err) {
-        console.error('Error verifying webhook:', err)
-        return new Response('Error occured', {
-            status: 400
-        })
-    }
-
-    //GET ID AND TYPE
-    const eventType = evt.type
-
-    if (eventType === "user.created") {
-        await db.user.create({
-            data: {
-                externalUserId: payload.data.id,
-                username: payload.data.username,
-                imageUrl: payload.data.image_url
-            }
-        })
-    }
-
-    if (eventType === "user.updated") {
-        const currentUser = await db.user.findUnique({
-            where: {
-                externalUserId: payload.data.id
-            }
-        })
-
-        if (!currentUser) {
-            return new Response("User not found", {status: 404})
-        }
-
-        await db.user.update({
-            where: {
-                externalUserId: payload.data.id,
-            },
-            data: {
-                username: payload.data.username,
-                imageUrl: payload.data.image_url,
-            }
-        })
-    }
-
-    if (eventType === "user.deleted") {
-        await db.user.delete({
-            where: {
-                externalUserId: payload.data.id
-            }
-        })
-    }
-
-    return new Response('', {status: 200})
+  return new Response("", { status: 200 });
 }
-
