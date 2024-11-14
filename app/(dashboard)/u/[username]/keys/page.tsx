c9568916ba6518d10c11dd92c1630a0b1ea0acf8
mateusz-wojnar <mwojnar87@gmail.com>
2024-11-14 18:42:18 +0100

    Implemented connection generation dialog using alert dialog and select

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -4,6 +4,7 @@ import { UrlCard } from "./_components/url-card";
 import { getSelf } from "@/lib/auth-service";
 import { getStreamByUserId } from "@/lib/stream-service";
 import { KeyCard } from "./_components/key-card";
+import { ConnectModal } from "./_components/connect-modal";
 
 const KeysPage = async () => {
   const self = await getSelf();
@@ -17,7 +18,7 @@ const KeysPage = async () => {
     <div className="p-6">
       <div className="flex items-center justify-between mb-4">
         <h1 className="text-2xl font-bold">Keys & URLs</h1>
-        <Button variant="primary">Generate</Button>
+        <ConnectModal></ConnectModal>
       </div>
       <div className="space-y-4">
         <UrlCard value={stream.serverUrl} />
