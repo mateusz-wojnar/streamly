2c39e07f6165d5bef40c4af12e0c75b2f0951b57
mateusz-wojnar <mwojnar87@gmail.com>
2024-11-17 22:30:17 +0100

    Implemented part of chat UI and funcionality

---
+++ components/stream-player/chat-form.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface ChatFormProps {
+  onSubmit: () => void;
+  value: string;
+  onChange: (value: string) => void;
+  isHidden: boolean;
+  isFollowersOnly: boolean;
+  isFollowing: boolean;
+  isDelayed: boolean;
+}
+
+export const ChatForm = ({
+  onSubmit,
+  value,
+  onChange,
+  isHidden,
+  isFollowersOnly,
+  isFollowing,
+  isDelayed,
+}: ChatFormProps) => {
+  return <div>Chat</div>;
+};
