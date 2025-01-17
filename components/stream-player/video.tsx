98928b37609fff04a92bf58a0bcb93d227c8c1ba
mateusz-wojnar <mwojnar87@gmail.com>
2025-01-17 21:01:34 +0100

    Previous commit fix

--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -6,6 +6,8 @@ import {
   useRemoteParticipant,
   useTracks,
 } from "@livekit/components-react";
+
+import { Skeleton } from "@/components/ui/skeleton";
 import { OfflineVideo } from "./offline-video";
 import { LoadingVideo } from "./loading-video";
 import { LiveVideo } from "./live-video";
@@ -34,4 +36,12 @@ export const Video = ({ hostIdentity, hostName }: VideoProps) => {
   }
 
   return <div className="aspect-video border-b group relative">{content}</div>;
+};
+
+export const VideoSkeleton = () => {
+  return (
+    <div className="aspect-video border-x border-background">
+      <Skeleton className="h-full w-full rounded-none" />
+    </div>
+  );
 };
