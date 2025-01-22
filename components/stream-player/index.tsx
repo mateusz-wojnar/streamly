b96840709641390964212c81acca3678edd470ec
mateusz-wojnar <mwojnar87@gmail.com>
2025-01-22 15:53:57 +0100

    Resolved security issue (modified getUserByUsername)

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
 
 import { cn } from "@/lib/utils";
@@ -14,12 +12,28 @@ import { ChatToggle } from "./chat-toggle";
 import { InfoCard } from "./info-card";
 import { AboutCard } from "./about-card";
 
+type CustomStream = {
+  id: string;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+  isLive: boolean;
+  thumbnailUrl: string | null;
+  name: string;
+};
+
+type CustomUser = {
+  id: string;
+  username: string;
+  bio: string | null;
+  stream: CustomStream | null;
+  imageUrl: string;
+  _count: { followedBy: number };
+};
+
 interface StreamPlayerProps {
-  user: User & {
-    stream: Stream | null;
-    _count: { followedBy: number };
-  };
-  stream: Stream;
+  user: CustomUser;
+  stream: CustomStream;
   isFollowing: boolean;
 }
 
